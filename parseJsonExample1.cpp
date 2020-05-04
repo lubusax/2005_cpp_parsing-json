334dcac134fc556ee8c9acb4d9b966065c89e864
lubusax <lu.bu.sax@gmail.com>
2020-05-04 11:30:22 +0200

    thingsGate.json first tests ok :)

--- parseJsonExample1.cpp
+++ parseJsonExample1.cpp
@@ -7,10 +7,9 @@
 
 void decode()
 {
-    //std::string text ="{ \"people\": [{\"id\": 1, \"name\":\"MIKE\",\"surname\":\"TAYLOR\"}, {\"id\": 2, \"name\":\"TOM\",\"surname\":\"JERRY\"} ]}";
     Json::Value root;
     Json::Reader reader;
-    const char * file = "output1.json";
+    const char * file = "thingsGate.json";
     std::fstream inFile1 {file,std::ios::in};
     bool parsingSuccessful = reader.parse(inFile1, root );
     inFile1.close();
@@ -19,22 +18,28 @@ void decode()
         std::cout << "Error parsing the string" << std::endl;
     }
 
-    const Json::Value mynames = root["people"];
+    const Json::Value displayCustomizing = root["thingsGate"]["display"]["customizing"];
     
-    std::cout << mynames.size() << std::endl;
+    //std::cout << thingsGate.size() << std::endl;
 
-    for ( int index = 0; index < mynames.size(); ++index )
-    {
-        std::cout << mynames[index]["id"] << std::endl;
-    }
-    std::cout << mynames << std::endl;
+    // for ( int index = 0; index < thingsGate.size(); ++index )
+    // {
+    //     std::cout << thingsGate[index] << std::endl;
+    // }
 
-    Json::StyledStreamWriter writer;
-    file = "output1.json";
-    std::fstream outFile1 {file,std::ios::out};
-    //outFile1.open();
-    writer.write(outFile1,root);
-    outFile1.close();
+    const Json::Value   activeScreenOption 
+                        = displayCustomizing["screen"]["active"];
+    int i               = activeScreenOption.asLargestInt();
+    std::cout << 
+        displayCustomizing["screen"]["options"][i]
+             << std::endl;
+
+    // Json::StyledStreamWriter writer;
+    // file = "output1.json";
+    // std::fstream outFile1 {file,std::ios::out};
+    // //outFile1.open();
+    // writer.write(outFile1,root);
+    // outFile1.close();
 }
 
 
