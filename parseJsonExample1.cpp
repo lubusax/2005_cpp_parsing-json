319f132de7688a1d9dbed949b75bc56522a50705
lubusax <lu.bu.sax@gmail.com>
2020-05-04 09:12:45 +0200

    read from and write to file - ok :)

--- parseJsonExample1.cpp
+++ parseJsonExample1.cpp
@@ -1,15 +1,19 @@
 #include <string>
 #include <iostream>
+#include <fstream>
 #include <json/json.h>
 
 //using namespace std;
 
 void decode()
 {
-    std::string text ="{ \"people\": [{\"id\": 1, \"name\":\"MIKE\",\"surname\":\"TAYLOR\"}, {\"id\": 2, \"name\":\"TOM\",\"surname\":\"JERRY\"} ]}";
+    //std::string text ="{ \"people\": [{\"id\": 1, \"name\":\"MIKE\",\"surname\":\"TAYLOR\"}, {\"id\": 2, \"name\":\"TOM\",\"surname\":\"JERRY\"} ]}";
     Json::Value root;
     Json::Reader reader;
-    bool parsingSuccessful = reader.parse( text, root );
+    const char * file = "output1.json";
+    std::fstream inFile1 {file,std::ios::in};
+    bool parsingSuccessful = reader.parse(inFile1, root );
+    inFile1.close();
     if ( !parsingSuccessful )
     {
         std::cout << "Error parsing the string" << std::endl;
@@ -21,13 +25,16 @@ void decode()
 
     for ( int index = 0; index < mynames.size(); ++index )
     {
-        std::cout << mynames[index] << std::endl;
+        std::cout << mynames[index]["id"] << std::endl;
     }
+    std::cout << mynames << std::endl;
 
-// Json::Value value1;
-// value1 = mynames.get("name", "default");
-//     std::cout << value1 << std::endl;
-
+    Json::StyledStreamWriter writer;
+    file = "output1.json";
+    std::fstream outFile1 {file,std::ios::out};
+    //outFile1.open();
+    writer.write(outFile1,root);
+    outFile1.close();
 }
 
 
